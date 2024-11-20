e0d10351cedb2d237b64625ba58147a430a9a542
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-11-20 12:01:13 +0100

    Création d'un nouveau projet versionné

---
+++ LightOff_GAUBIL_FABRE_version_console/src/lightoff_gaubil_fabre_version_console/GrilleDeJeu.java
@@ -0,0 +1,95 @@
+/*
+ */
+package lightoff_gaubil_fabre_version_console;
+
+/**
+ *
+ * @author vtino
+ */
+import java.util.Random;
+
+public class GrilleDeJeu {
+    private CelluleLumineuse[][] matriceCellules;
+    private int nbLignes;
+    private int nbColonnes;
+
+    public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
+        this.nbLignes = p_nbLignes;
+        this.nbColonnes = p_nbColonnes;
+        this.matriceCellules = new CelluleLumineuse[nbLignes][nbColonnes];
+
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j] = new CelluleLumineuse();
+            }
+        }
+    }
+
+    public void eteindreToutesLesCellules() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j].eteindreCellule();
+            }
+        }
+    }
+
+    public void activerLigneDeCellules(int idLigne) {
+        for (int j = 0; j < nbColonnes; j++) {
+            matriceCellules[idLigne][j].activerCellule();
+        }
+    }
+
+    public void activerColonneDeCellules(int idColonne) {
+        for (int i = 0; i < nbLignes; i++) {
+            matriceCellules[i][idColonne].activerCellule();
+        }
+    }
+
+    public void activerDiagonaleDescendante() {
+        for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
+            matriceCellules[i][i].activerCellule();
+        }
+    }
+
+    public void activerDiagonaleMontante() {
+        for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
+            matriceCellules[i][nbColonnes - 1 - i].activerCellule();
+        }
+    }
+
+    public void melangerMatriceAleatoirement(int nbTours) {
+        Random rand = new Random();
+        for (int t = 0; t < nbTours; t++) {
+            int choix = rand.nextInt(4);
+            switch (choix) {
+                case 0 -> activerLigneDeCellules(rand.nextInt(nbLignes));
+                case 1 -> activerColonneDeCellules(rand.nextInt(nbColonnes));
+                case 2 -> activerDiagonaleDescendante();
+                case 3 -> activerDiagonaleMontante();
+            }
+        }
+    }
+
+    public boolean cellulesToutesEteintes() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                if (!matriceCellules[i][j].estEteint()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                sb.append(matriceCellules[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
