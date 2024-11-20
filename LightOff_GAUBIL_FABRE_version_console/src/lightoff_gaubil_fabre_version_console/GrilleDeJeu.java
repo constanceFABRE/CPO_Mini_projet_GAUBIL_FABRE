f5a4ac89ef9a622c139269ee9723b146ba99a3e6
Constance FABRE <votre_mail>
2024-11-20 12:35:05 +0100

    Création d'un nouveau projet versionné

--- LightOff_GAUBIL_FABRE_version_console/src/lightoff_gaubil_fabre_version_console/GrilleDeJeu.java
+++ LightOff_GAUBIL_FABRE_version_console/src/lightoff_gaubil_fabre_version_console/GrilleDeJeu.java
@@ -44,52 +44,5 @@ public class GrilleDeJeu {
             matriceCellules[i][idColonne].activerCellule();
         }
     }
-
-    public void activerDiagonaleDescendante() {
-        for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
-            matriceCellules[i][i].activerCellule();
-        }
-    }
-
-    public void activerDiagonaleMontante() {
-        for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
-            matriceCellules[i][nbColonnes - 1 - i].activerCellule();
-        }
-    }
-
-    public void melangerMatriceAleatoirement(int nbTours) {
-        Random rand = new Random();
-        for (int t = 0; t < nbTours; t++) {
-            int choix = rand.nextInt(4);
-            switch (choix) {
-                case 0 -> activerLigneDeCellules(rand.nextInt(nbLignes));
-                case 1 -> activerColonneDeCellules(rand.nextInt(nbColonnes));
-                case 2 -> activerDiagonaleDescendante();
-                case 3 -> activerDiagonaleMontante();
-            }
-        }
-    }
-
-    public boolean cellulesToutesEteintes() {
-        for (int i = 0; i < nbLignes; i++) {
-            for (int j = 0; j < nbColonnes; j++) {
-                if (!matriceCellules[i][j].estEteint()) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < nbLignes; i++) {
-            for (int j = 0; j < nbColonnes; j++) {
-                sb.append(matriceCellules[i][j]).append(" ");
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
 }
+    
