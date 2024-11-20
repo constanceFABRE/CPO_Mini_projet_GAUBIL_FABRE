4b066320c09cab4ebeaea25994c7fe8a71cdc5cd
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-11-20 12:39:19 +0100

    Création d'un nouveau projet versionné

--- LightOff_GAUBIL_FABRE_version_console/src/lightoff_gaubil_fabre_version_console/LightOff_GAUBIL_FABRE_version_console.java
+++ LightOff_GAUBIL_FABRE_version_console/src/lightoff_gaubil_fabre_version_console/LightOff_GAUBIL_FABRE_version_console.java
@@ -9,13 +9,6 @@ package lightoff_gaubil_fabre_version_console;
  * @author constancefabre
  */
 public class LightOff_GAUBIL_FABRE_version_console {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
-        
     }
-//    
 }
