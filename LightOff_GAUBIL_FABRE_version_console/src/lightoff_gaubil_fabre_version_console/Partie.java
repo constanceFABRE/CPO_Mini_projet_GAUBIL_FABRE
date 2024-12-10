36e5c83daf050c850aa0cfa96a2cbbe6c554ade1
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-12-10 18:58:30 +0100

    ajout de l'utilisation d'un joker, modification de la grille de jeu et de la classe Partie.

--- LightOff_GAUBIL_FABRE_version_console/src/lightoff_gaubil_fabre_version_console/Partie.java
+++ LightOff_GAUBIL_FABRE_version_console/src/lightoff_gaubil_fabre_version_console/Partie.java
@@ -11,12 +11,41 @@ import java.util.Scanner;
 
 public class Partie {
     private GrilleDeJeu grille;
-    private int nbCoups; // permet de svoir avec combien de coups on a gangé la partie 
+    private int nbCoups;   // permet de svoir avec combien de coups on a gangé la partie 
+    private int coupsRestants; // Limite de coups
+    private boolean jokerUtilise; // Indique si le joker a été utilisé
 
-    public Partie(int taille) {
+
+    public Partie(int taille,int maxCoups) {
         this.grille = new GrilleDeJeu(taille, taille);// permet de créer la grille de taille taille X taille 
         this.nbCoups = 0;
+        this.coupsRestants = maxCoups;
+        this.jokerUtilise = false; // Le joueur commence sans avoir utilisé le joker
+
         grille.melangerMatriceAleatoirement(5);// permet de mélanger la grille
+    }
+
+    
+        // Méthode pour choisir une difficulté
+    public static Partie choisirDifficulte() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choisissez un niveau de difficulté :");
+        System.out.println("1. Débutant (5x5, 25 coups max)");
+        System.out.println("2. Confirmé (7x7, 18 coups max)");
+        System.out.println("3. Expert (10x10, 10 coups max)");
+
+        int choix = scanner.nextInt();
+        switch (choix) {
+            case 1:
+                return new Partie(5, 25); // Débutant
+            case 2:
+                return new Partie(7, 18); // Confirmé
+            case 3:
+                return new Partie(10, 10); // Expert
+            default:
+                System.out.println("Choix invalide, niveau Débutant par défaut.");
+                return new Partie(5, 25);
+        }
     }
 
     public void lancerPartie() {
