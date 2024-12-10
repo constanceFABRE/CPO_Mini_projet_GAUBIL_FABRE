966e43ea291d110a9a8d454fbb30464427294d2c
Constance FABRE <votre_mail>
2024-12-10 18:18:12 +0100

    changement de lumiere, pour mieux voir les cases et que ca soit plus réaliste

--- LightOff_GAUBIL_FABRE_version_graphique)/src/lightoff_gaubil_fabre_version_console/CelluleGraphique.java
+++ LightOff_GAUBIL_FABRE_version_graphique)/src/lightoff_gaubil_fabre_version_console/CelluleGraphique.java
@@ -1,5 +1,6 @@
 package lightoff_gaubil_fabre_version_console;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JButton;
 
@@ -28,6 +29,14 @@ public class CelluleGraphique extends JButton {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.setText(celluleLumineuseAssociee.toString());
+        int w = this.getWidth();
+        int h = this.getHeight();
+        if (celluleLumineuseAssociee.estEteint()== true){
+            g.setColor(Color.black);
+        }else{
+            g.setColor(Color.yellow);
+        }
+        g.fillOval(2, 2,w - 4, h - 4);
     }
 }
    
